09cf37498708ec26ba7b67702b16065293519d97
juliePreperier <julie.preperier@students.hevs.ch>
2017-04-04 11:25:21 +0200

    update class base

---
+++ app/src/main/java/com/example/comicexchange/ExplorerFavorisActivity.java
@@ -0,0 +1,13 @@
+package com.example.comicexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExplorerFavorisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explorer_favoris);
+    }
+}
