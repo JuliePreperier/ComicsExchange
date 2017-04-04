17dd169abc5c4e3b75c48a3b1709517603634bc2
Sandy <sandy.millius@students.hevs.ch>
2017-04-04 10:53:06 +0200

    Coucou

--- app/src/main/java/com/example/comicexchange/LogInActivity.java
+++ app/src/main/java/com/example/comicexchange/LogInActivity.java
@@ -7,10 +7,13 @@ import java.io.Console;
 
 public class LogInActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("putain de test a la con");
         setContentView(R.layout.activity_main);
+        System.out.println("salut");
     }
 }
