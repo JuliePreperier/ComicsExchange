3e15f29e34a13bc55492a93cd67bad2817b1bda1
juliePreperier <julie.preperier@students.hevs.ch>
2017-04-04 10:59:04 +0200

    retour initial

--- app/src/main/java/com/example/comicexchange/LogInActivity.java
+++ app/src/main/java/com/example/comicexchange/LogInActivity.java
@@ -10,11 +10,6 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("putain de test a la con");
-        System.out.println("merde");
-        System.out.println("yop");
-        System.out.println("Merge test");
         setContentView(R.layout.activity_main);
-        System.out.println("casse couille");
     }
 }
