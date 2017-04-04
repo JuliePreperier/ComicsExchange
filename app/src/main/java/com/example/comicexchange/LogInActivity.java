1c064dbdf66aaf5ef943770a33c38111ef7f5b7d
juliePreperier <julie.preperier@students.hevs.ch>
2017-04-04 10:42:31 +0200

    First Change -> hello world in Main activity

--- app/src/main/java/com/example/comicexchange/LogInActivity.java
+++ app/src/main/java/com/example/comicexchange/LogInActivity.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 
 import java.io.Console;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("Hello world");
         setContentView(R.layout.activity_main);
     }
 }
