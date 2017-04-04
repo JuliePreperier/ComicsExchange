a204f991725e4ee5b96b8832ee36f6e35f6a06bf
juliePreperier <julie.preperier@students.hevs.ch>
2017-04-04 10:50:23 +0200

    test

--- app/src/main/java/com/example/comicexchange/LogInActivity.java
+++ app/src/main/java/com/example/comicexchange/LogInActivity.java
@@ -10,11 +10,6 @@ public class LogInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("salut");
-        System.out.println("salut2");
-        System.out.println("salut3");
-        System.out.println("salut4");
-        System.out.println("Modif2");
         setContentView(R.layout.activity_main);
     }
 }
