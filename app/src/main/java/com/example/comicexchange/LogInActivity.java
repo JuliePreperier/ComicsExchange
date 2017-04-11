43602045bb56b7b71e064e939a1efdfc5b54df27
juliePreperier <julie.preperier@students.hevs.ch>
2017-04-11 11:36:45 +0200

    DB implementation

--- app/src/main/java/com/example/comicexchange/LogInActivity.java
+++ app/src/main/java/com/example/comicexchange/LogInActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import java.io.Console;
 
+import BDD.DbHelper;
+
 public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DbHelper myDBHelper = new DbHelper(this); // L'Activité est le Context qu'il faut donner
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
