3af1bbd9d128972439408a605a598537ee92a421
Sandy <sandy.millius@students.hevs.ch>
2017-04-25 15:09:58 +0200

    Design layout 11.04

--- app/src/main/java/com/example/comicexchange/LogInActivity.java
+++ app/src/main/java/com/example/comicexchange/LogInActivity.java
@@ -13,6 +13,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         DbHelper myDBHelper = new DbHelper(this); // L'Activité est le Context qu'il faut donner
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_explorer);
     }
 }
